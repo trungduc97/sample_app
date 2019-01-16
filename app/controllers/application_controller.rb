efd7fb71905d519b7bcc04dc8da8743234692336
trungduc97 <ducanhtran010@gmail.com>
2019-01-16 15:30:56 +0700

    chapter 5: Filling in the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = if I18n.available_locales.include?(locale)
+                    locale
+                  else
+                    I18n.default_locale
+                  end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
