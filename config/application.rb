33be44ae2cab5698685f2a72aec646899ecdc5c7
trungduc97 <ducanhtran010@gmail.com>
2019-01-16 15:30:56 +0700

    Improve the README

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
   end
 end
